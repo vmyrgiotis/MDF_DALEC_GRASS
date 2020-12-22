40830cd7bf3afa92aaae62c1c81ef03a4a049d8e
Vasilis <38428015+vmyrgiotis@users.noreply.github.com>
2020-12-22 09:27:58 +0000

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup, find_packages
+
+HERE = pathlib.Path(__file__).parent
+
+VERSION = '0.1.0'
+PACKAGE_NAME = 'MDF DALEC Grass'
+AUTHOR = 'Vasilis Myrgiotis'
+AUTHOR_EMAIL = 'v.myrgioti@ed.ac.uk'
+URL = 'https://github.com/vmyrgiotis/MDF_DALEC_Grass'
+
+LICENSE = 'MIT'
+DESCRIPTION = 'A Bayesian model-data fusion algorithm for simulating carbon dynamics in grassland ecosystems'
+LONG_DESCRIPTION = (HERE / "README.md").read_text()
+LONG_DESC_TYPE = "text/markdown"
+
+INSTALL_REQUIRES = ["numpy", "pandas","spotpy","sklearn","sentinelhub", "shapely", "datetime", "glob", "subprocess", "geopandas", "cdsapi",'mpi4py']
+PYTHON_REQUIRES = '>=3.8'
+
+setup(name=PACKAGE_NAME,
+	version=VERSION,
+	description=DESCRIPTION,
+	long_description=LONG_DESCRIPTION,
+	long_description_content_type=LONG_DESC_TYPE,
+	author=AUTHOR,
+	license=LICENSE,
+	author_email=AUTHOR_EMAIL,
+	url=URL,
+	install_requires=INSTALL_REQUIRES,
+	packages=find_packages()
+	)
